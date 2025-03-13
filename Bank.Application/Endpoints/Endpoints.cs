1b3ad7217e22b8131e11527217e7e167e563bade
Uroš Čolić <ucolic3021rn@raf.rs>
2025-03-13 18:44:17 +0100

    Implement Enpoints for TransactionCode

--- Bank.Application/Endpoints/Endpoints.cs
+++ Bank.Application/Endpoints/Endpoints.cs
@@ -154,4 +154,14 @@ public static partial class Endpoints
     }
 }
 
+public static partial class Endpoints
+{
+    public static class TransactionCode
+    {
+        public const string Base   = $"{ApiBase}/transaction/codes";
+        public const string GetOne = $"{Base}/{{id:guid}}";
+        public const string GetAll = $"{Base}";
+    }
+}
+
 #endregion
