58c61ffdd5f90708475f611f1c97fd00e136721f
NikolaRadovicTihi <114955835+NikolaRadovicTihi@users.noreply.github.com>
2025-03-15 18:07:22 +0100

    Initial service implementation for loan. Added the Installment request and mapper as well

---
+++ Bank.Application/Requests/InstallmentRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Bank.Application.Requests;
+
+public class InstallmentRequest
+{
+    public required Guid     LoanId          { get; set; }
+    public required decimal  InterestRate    { get; set; }
+    public required DateOnly ExpectedDueDate { get; set; }
+    public required DateOnly ActualDueDate   { get; set; }
+    public required int      Status          { get; set; }
+}
